8365e2d9eba0dc74518e1dd36db31339b2ed2d38
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-03-22 13:50:01 -0400

    Added variable with quiz questions, their options and correct answer. Also added draft of code for timer.

---
+++ script.js
@@ -0,0 +1,37 @@
+// Quiz timer
+// let i = 0;
+// var increment = function () {
+//     i++;
+//     console.log(i);
+// };
+
+// setTimeout() {
+//     i++;
+//     console.log(i);
+//     59 * 1000;
+// }
+
+var questions = [{
+    question: "How many columns can you have in a row?",
+    answers: ["16", "8", "12", "4"],
+    correctAnswer: "12",
+}, {
+    question: "What is used to create dynamic web apps that take in user inputs, animate elements & much more?",
+    answers: ["CSS", "Javascript", "Bootstrap", "GitBash"],
+    correctAnswer: "JavaScript",
+}, {
+    question: "Indexes always start with what number?",
+    answers: ["A", "1", "100", "0"],
+    correctAnswer: "0",
+},
+{
+    question: "What is a set of code features that developers can use in their app to interact with components of a user's web browser, data sets, hardware/software on a user's computer?",
+    answers: ["API", "HTML", "HTTPS", "Bootstrap"],
+    correctAnswer: "API",
+},
+{
+    question: "What function stops additional nested events from firing off?",
+    answers: ["Stop Propogation", "Stop Javascript", "Stop HTML", "Stop Load"],
+    correctAnswer: "Stop Propogation",
+},
+];
