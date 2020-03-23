4bfeebdf74772db0c20da12d77ebcbce3dd07603
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-03-23 11:21:52 -0400

    updates

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ var startBtn = document.querySelector("#start-button");
 var quizRules = document.querySelector("#quiz-rules");
 var quizSectionDiv = document.querySelector("#quiz-section")
 var quizQuestionDiv = document.querySelector("#quiz-question");
+var option1Btn = document.querySelector("#Answer-1");
+var option2Btn = document.querySelector("#Answer-2");
+var option3Btn = document.querySelector("#Answer-3");
+var option4Btn = document.querySelector("#Answer-4");
 var nextButton = document.querySelector("#next-button");
+var timerEl = document.querySelector("#navbar-timer");
+var secondsLeft = 60;
+var timerInterval;
+var currentQuestionPosition = 0;
 
 // Add event listener to "Start Quiz" button
 startBtn.addEventListener("click", startQuiz)
@@ -21,12 +29,33 @@ function startQuiz() {
     setNextQuestion()
 }
 
+// Function to start timer
+function startTimer() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
+}
+
+function sendMessage() {
+    timeEl.textContent = "Time's Up!";
+}
+
+
 // Add event listener to "Next" button
 nextButton.addEventListener("click", setNextQuestion)
 
 // Update quiz with next question
 function setNextQuestion() {
-    showQuestion()
+    quizQuestionDiv.textContent = questions[0].question;
+    option1Btn.textContent = questions[0].options[0];
+    option2Btn.textContent = questions[1].options[1];
+    option3Btn.textContent = questions[2].options[2];
+    option4Btn.textContent = questions[3].options[3];
 }
 
 function showQuestion(question) {
@@ -35,29 +64,55 @@ function showQuestion(question) {
 
 var questions = [{
     question: "How many columns can you have in a row?",
-    options: ["16", "8", "12", "4"],
+    options: {
+        option1: ["16", "incorrect"],
+        option2: ["8", "incorrect"],
+        option3: ["12", "correct"],
+        option4: ["4", "incorrect"]
+    },
     correctAnswer: "12",
 }, {
     question: "What is used to create dynamic web apps that take in user inputs, animate elements & much more?",
-    options: ["CSS", "Javascript", "Bootstrap", "GitBash"],
+    options: {
+        option1: ["CSS", "incorrect"],
+        option2: ["Javascript", "correct"],
+        option3: ["Bootstrap", "incorrect"],
+        option4: ["GitBash", "incorrect"]
+    },
     correctAnswer: "JavaScript",
 }, {
     question: "Indexes always start with what number?",
-    options: ["A", "1", "100", "0"],
+    options: {
+        option1: ["A", "incorrect"],
+        option2: ["1", "incorrect"],
+        option3: ["100", "incorrect"],
+        option4: ["0", "incorrect"]
+    },
     correctAnswer: "0",
 },
 {
     question: "What is a set of code features that developers can use in their app to interact with components of a user's web browser, data sets, hardware/software on a user's computer?",
-    options: ["API", "HTML", "HTTPS", "Bootstrap"],
+    options: {
+        option1: ["API", "correct"],
+        option2: ["HTML", "incorrect"],
+        option3: ["HTTPS", "incorrect"],
+        option4: ["Bootstrap", "incorrect"]
+    },
     correctAnswer: "API",
 },
 {
     question: "What function stops additional nested events from firing off?",
-    options: ["Stop Propogation", "Stop Javascript", "Stop HTML", "Stop Load"],
+    options: {
+        option1: ["Stop Propogation", "correct"],
+        option2: ["Stop Javascript", "incorrect"],
+        option3: ["Stop HTML", "incorrect"],
+        option4: ["Stop Load", "incorrect"]
+    },
     correctAnswer: "Stop Propogation",
 },
 ];
 
+// function selectAnswer() {
 
 // var correctAnswerCount = 0;
 // var finalScore = correctAnswerCount * 20;
@@ -72,7 +127,7 @@ var questions = [{
 //     }
 // }
 
-// function selectAnswer() {
+
 
 // }
 // Quiz timer
