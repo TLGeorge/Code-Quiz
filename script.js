f950cfee7950b021a80b5203345e2a96def4518c
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-03-22 21:37:12 -0400

    Updated index page with Quiz title, decription and 'start quiz' button.Also added som js code.

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+//Creating a function for "Start Quiz" button
+var startBtn = document.querySelector("#start");
+
+function takeQuiz() {
+
+    var questions = [{
+        question: "How many columns can you have in a row?",
+        options: ["16", "8", "12", "4"],
+        correctAnswer: "12",
+    }, {
+        question: "What is used to create dynamic web apps that take in user inputs, animate elements & much more?",
+        options: ["CSS", "Javascript", "Bootstrap", "GitBash"],
+        correctAnswer: "JavaScript",
+    }, {
+        question: "Indexes always start with what number?",
+        options: ["A", "1", "100", "0"],
+        correctAnswer: "0",
+    },
+    {
+        question: "What is a set of code features that developers can use in their app to interact with components of a user's web browser, data sets, hardware/software on a user's computer?",
+        options: ["API", "HTML", "HTTPS", "Bootstrap"],
+        correctAnswer: "API",
+    },
+    {
+        question: "What function stops additional nested events from firing off?",
+        options: ["Stop Propogation", "Stop Javascript", "Stop HTML", "Stop Load"],
+        correctAnswer: "Stop Propogation",
+    },
+    ];
+
+
+    var correctAnswerCount = 0;
+    var finalScore = correctAnswerCount * 20;
+
+    for (var i = 0; i < questions.length; i++) {
+        const answer = questions[i];
+        if (answer === questions[i].correctAnswer) {
+            score++;
+            return ("Correct!");
+        } else {
+            return ("Wrong!");
+        }
+    }
 // Quiz timer
 // let i = 0;
 // var increment = function () {
@@ -11,27 +54,4 @@
 //     59 * 1000;
 // }
 
-var questions = [{
-    question: "How many columns can you have in a row?",
-    answers: ["16", "8", "12", "4"],
-    correctAnswer: "12",
-}, {
-    question: "What is used to create dynamic web apps that take in user inputs, animate elements & much more?",
-    answers: ["CSS", "Javascript", "Bootstrap", "GitBash"],
-    correctAnswer: "JavaScript",
-}, {
-    question: "Indexes always start with what number?",
-    answers: ["A", "1", "100", "0"],
-    correctAnswer: "0",
-},
-{
-    question: "What is a set of code features that developers can use in their app to interact with components of a user's web browser, data sets, hardware/software on a user's computer?",
-    answers: ["API", "HTML", "HTTPS", "Bootstrap"],
-    correctAnswer: "API",
-},
-{
-    question: "What function stops additional nested events from firing off?",
-    answers: ["Stop Propogation", "Stop Javascript", "Stop HTML", "Stop Load"],
-    correctAnswer: "Stop Propogation",
-},
-];
+
